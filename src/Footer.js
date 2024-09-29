1fa48017b5e2ade350b210261319e6c662866aa7
Brendan Munnelly <munnellyb@gmail.com>
2024-09-29 02:21:26 +0100

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Created by <b>munnelly.com/frontend</b></p>
+    </div>
+  );
+};
+
+export default Footer;
